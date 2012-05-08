4ae7d9f524a7d9100f025adb14d03f4a6cefe820
Achim Friedland <github@ahzf.de>
2012-05-08 03:32:28 +0200

    Updates...

---
+++ OdinTests/UnitTests.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright (c) 2010-2012, Achim 'ahzf' Friedland <achim@graph-database.org>
+ * This file is part of Odin <http://www.github.com/Vanaheimr/Odin>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using de.ahzf.Illias.Commons;
+using de.ahzf.Blueprints;
+using de.ahzf.Blueprints.PropertyGraphs;
+using de.ahzf.Balder;
+
+using NUnit.Framework;
+
+#endregion
+
+namespace de.ahzf.Vanaheimr.Odin.UnitTests
+{
+
+    [TestFixture]
+    public class UnitTests
+    {
+
+        [Test]
+        public void testCreateGenericPropertyGraph()
+        {
+            var _graph = GraphFactory.CreateGenericPropertyGraph(1);
+            Assert.NotNull(_graph);
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+
+[Odin](http://github.com/Vanaheimr/Odin) is a little helper project to support your first steps
+with the Vanaheimr graph processing stack. It comes with all neccessary Vanaheimr subprojects as
+git submodules to make your life easier.
+
+    git submodule init
+	git submodule update
+	
+In order to update the submodules to their latest version type:	
+	
+	git submodule foreach git pull -q origin master
+
